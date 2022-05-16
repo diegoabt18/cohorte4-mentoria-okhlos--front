463601409c463a14a017c4dd10f8d9dab3d4bf3e
JFJ1972 <97111500+JFJ1972@users.noreply.github.com>
2022-05-16 18:49:41 -0500

    cleanprueba

--- src/components/Prueba.jsx
+++ src/components/Prueba.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
-import BasicButton from './atoms/BasicButton'
-import BasicInputText from './atoms/BasicInputText'
-import ImageLogin from './atoms/ImageLogin'
-import LinkForgetPassword from './atoms/LinkForgetPassword'
-import LogoEducamas from './atoms/LogoEducamas'
+
 
 const Prueba = () => {
   return (
     <div>
-        <BasicButton/>
-        <BasicInputText/>
-        <LogoEducamas/>
-        <LinkForgetPassword/>
-        <ImageLogin/>
+        
     </div>
   )
 }
