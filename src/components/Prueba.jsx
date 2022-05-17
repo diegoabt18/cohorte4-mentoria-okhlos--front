ad45d576e75ff836bd41c2ffabf10107a8d96d77
JFJ1972 <97111500+JFJ1972@users.noreply.github.com>
2022-05-17 17:05:24 -0500

    atomos13-20

--- src/components/Prueba.jsx
+++ src/components/Prueba.jsx
@@ -1,31 +1,11 @@
 import React from 'react'
-import BtnEditTable from './atoms/BtnEditTable'
-import ButtonEscuela from './atoms/ButtonEscuela'
-import ButtonIconText from './atoms/ButtonIconText'
-import CardInfo from './atoms/CardInfo'
-import IconCard from './atoms/IconCard'
-import ImgBienvenidaAdmin from './atoms/ImgBienvenidaAdmin'
-import Rol from './atoms/Rol'
-import Search from './atoms/Search'
-import TitleAddEstudiante from './atoms/TitleAddEstudiante'
-import TitlePage from './atoms/TitlePage'
-import XCloseBtn from './atoms/XCloseBtn'
+
 
 
 const Prueba = () => {
   return (
     <div>
-        <IconCard/>
-        <CardInfo/>
-        <Search/>
-        <ButtonEscuela/>
-        <Rol/>
-        <TitlePage/>
-        <ButtonIconText/>
-        <ImgBienvenidaAdmin/>
-        <XCloseBtn/>
-        <TitleAddEstudiante/>
-        <BtnEditTable/>
+        
     </div>
   )
 }
