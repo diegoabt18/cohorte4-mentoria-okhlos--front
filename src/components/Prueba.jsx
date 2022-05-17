c8f7ce0dd8df1a7a4dea5fedaa7d210ec8d33af3
Diegoabt18 <diegoalbertbt@gmail.com>
2022-05-17 17:03:42 -0500

    Atomos del 6-10, 26, 28 y 30-32

--- src/components/Prueba.jsx
+++ src/components/Prueba.jsx
@@ -1,32 +1,10 @@
 import React from 'react'
-import BasicButtonIcon from './atoms/BasicButtonIcon'
-import PhotoProfile from './atoms/PhotoProfile'
-import PersonName from './atoms/PersonName'
-import LogoMenuSidebar from './atoms/LogoMenuSidebar'
-import LogoFooter from './atoms/LogoFooter'
-import Swal from 'sweetalert'
-import { ModalAlert } from './organisms/ModalAlert'
-import BasicComboBox from './atoms/BasicComboBox'
-import MultiComboBox from './atoms/MultiComboBox'
-import CloseModalButtonIcon from './atoms/CloseModalButtonIcon'
-import InputDate from './atoms/InputDate'
+
 const Prueba = () => {
   
   return (
     <div>
-        <BasicButtonIcon />
-        <PhotoProfile />
-        <PersonName />
-        <LogoMenuSidebar />
-        <LogoFooter />
-        <BasicComboBox />
-        <MultiComboBox />
-        <CloseModalButtonIcon />
-        <InputDate />
-          <br />
-          <br />
-          <br />
-          <br />
+ 
     </div>
   )
 }
