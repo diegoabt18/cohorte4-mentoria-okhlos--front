c57538beba8bee23207247f868855cf0ec0bfe04
JFJ1972 <97111500+JFJ1972@users.noreply.github.com>
2022-05-18 14:19:39 -0500

    molecula 3 y 4

--- src/components/molecules/DashboardOptions.jsx
+++ src/components/molecules/DashboardOptions.jsx
@@ -3,7 +3,7 @@ import { BasicButton, TextInfoDashBoard } from '../atoms'
 
 const DashboardOptions = () => {
   return (
-    <div className='flex gap-x-2 items-center justify-aroundgit'>
+    <div className='flex gap-x-2 items-center justify-around'>
         <TextInfoDashBoard/>
         <div className='flex gap-x-8 justify-between w-1/3' >
         <BasicButton/>
