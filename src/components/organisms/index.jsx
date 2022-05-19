9e9129b89b004f1275c5609ecb426caea3b9cb48
Diegoabt18 <diegoalbertbt@gmail.com>
2022-05-19 17:05:59 -0500

    actualizacion

--- src/components/organisms/index.jsx
+++ src/components/organisms/index.jsx
@@ -1,5 +1,5 @@
 export {default as ContainerLogin} from "./ContainerLogin";
-export {default as ContenedorPerfil} from './ContenedorPerfil'
-export {default as MenuDesktop} from './MenuDesktop'
-export {default as MenuCabeceraDesktop} from './MenuCabeceraDesktop'
-export {default as Welcome} from './Welcome'
+export {default as ContenedorPerfil} from "./ContenedorPerfil";
+export {default as MenuDesktop} from "./MenuDesktop";
+export {default as MenuCabeceraDesktop} from "./MenuCabeceraDesktop";
+export {default as Welcome} from "./Welcome";
